49a30ab5742b7d7330fc80e8ca23ae852439594c
Joe Czepil <joe.czepil@getsmartsolutions.ca>
2023-03-28 21:27:12 -0700

    Change get request for standards

--- api_request.py
+++ api_request.py
@@ -1,14 +1,33 @@
 import requests
 
-def call_api():
-    url = "https://94ce-2001-569-7ed1-e000-f5bb-205c-9c3b-7de.ngrok.io/api/standard-change/"
-    response = requests.get(url)
+def get_data():
+    try:
+        response = requests.get(
+            "https://94ce-2001-569-7ed1-e000-f5bb-205c-9c3b-7de.ngrok.io/api/standard-change/",
+            headers={
+                "Authorization": "",
+                "Content-Type": "application/json",
+            }
+        )
 
-    if response.status_code == 200:
-        print("GET request was successful.")
-        print("Response data:", response.json())
+        # Raise an exception if the response contains an HTTP error status code
+        response.raise_for_status()
+
+    except requests.exceptions.HTTPError as e:
+        print(f"An HTTP error occurred: {e}")
+        return None
+    except requests.exceptions.RequestException as e:
+        print(f"A request error occurred: {e}")
+        return None
     else:
-        print(f"GET request failed with status code {response.status_code}.")
+        return response.json()
 
 if __name__ == "__main__":
-    call_api()
+    data = get_data()
+
+    if data is not None:
+        print("Data retrieved from the API:")
+        print(data)
+    else:
+        print("Failed to retrieve data from the API.")
+
