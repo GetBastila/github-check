3b22861376d62a1e5d2363436386b95ccb4f6f1e
Joe Czepil <joe.czepil@getsmartsolutions.ca>
2023-04-01 10:46:50 -0700

    Fix Json format

--- api_request.py
+++ api_request.py
@@ -9,21 +9,36 @@ def get_data():
     repo = os.environ["REPO_NAME"]
 
     try:
+        print({
+            'pat': github_pat,
+            'repo': repo,
+            'owner': owner,
+            'branch': branch
+        })
+
+        # {
+        #     'pat': '123',
+        #     'repo': '123',
+        #     'owner': '123',
+        #     'branch': '123'
+        # }
+
         response = requests.post(
             "https://bastilaapi-production.up.railway.app/api/github-check/",
             headers={
                 "Content-Type": "application/json",
             },
             data={
-                'pat': github_pat,
-                'repo': repo,
-                'owner': owner,
-                'branch': branch
+                "pat": "123",
+                "repo": "123",
+                "owner": "123",
+                "branch": "123"
             }
         )
 
         response.raise_for_status()  # Raise an exception if the response contains an HTTP error status code
     except requests.exceptions.RequestException as e:
+        print(response.content)
         sys.exit(f"Error: {str(e)}")
 
 
