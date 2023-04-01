31fea9e0570a1e0e047b6e8966c48120cfc77780
Joe Czepil <joe.czepil@getsmartsolutions.ca>
2023-04-01 10:17:32 -0700

    Use new endpoint

--- api_request.py
+++ api_request.py
@@ -1,33 +1,32 @@
 import requests
 
 def get_data():
+    github_pat = os.environ["GITHUB_PAT"]
+    branch = os.environ["COMPARE_BRANCH"]
+    owner = os.environ["REPO_OWNER"]
+    repo = os.environ["REPO_NAME"]
+
     try:
-        response = requests.get(
-            "https://bastilaapi-production.up.railway.app/api/standard-change/",
+        response = requests.post(
+            "https://bastilaapi-production.up.railway.app/api/github-check/",
             headers={
-                "Authorization": "",
                 "Content-Type": "application/json",
+            },
+            data={
+                'pat': github_pat,
+                'repo': repo,
+                'owner': owner,
+                'branch': branch
             }
         )
 
-        # Raise an exception if the response contains an HTTP error status code
-        response.raise_for_status()
-
-    except requests.exceptions.HTTPError as e:
-        print(f"An HTTP error occurred: {e}")
-        return None
+        response.raise_for_status()  # Raise an exception if the response contains an HTTP error status code
     except requests.exceptions.RequestException as e:
-        print(f"A request error occurred: {e}")
-        return None
-    else:
-        return response.json()
+        sys.exit(f"Error: {str(e)}")
+
 
 if __name__ == "__main__":
     data = get_data()
 
-    if data is not None:
-        print("Data retrieved from the API:")
-        print(data)
-    else:
-        print("Failed to retrieve data from the API.")
+
 
